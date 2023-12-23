492882bcf804d7a2012455bbf017275fc1d72c2b
Marco Antonio <malirab2302@gmail.com>
2023-12-23 18:47:22 -0300

    App first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { QuotesList } from './components/QuotesList';
+import { QuotesForm } from './components/QuotesForms';
+import QuoteData from './model/quote'
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  padding: 2em 0;
+`
 
 const App: React.FC = () => {
+
+  const [ quotes, setQuotes ] = useState<QuoteData[]>([])
+
+  const addQuoteHandler = (quote: QuoteData): void => {
+    setQuotes(prevState => {
+      return [ ...prevState, quote ]
+    })
+  }
+
+  const removeQuoteHandler = (id: number): void => {
+    setQuotes(prevState => {
+
+      const filteredQuotes = prevState.filter(quote => quote.id !== id)
+
+      return filteredQuotes;
+    })
+  }
+
   return (
-    <div>
-      test
-    </div>
+    <StyledApp className='App'>
+
+      <QuotesForm onAddQuote={ addQuoteHandler }/>
+      <QuotesList quotes={quotes} onRemove={ removeQuoteHandler } />
+
+    </StyledApp>
   );
 }
 
